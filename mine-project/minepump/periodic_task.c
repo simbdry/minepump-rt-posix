96e0c76fe629ababf04173bf64e050b8f41c08d6
BEAUDRY Simon <s.beaudry@pcb61-12-15.isae.fr>
2022-12-14 17:49:37 +0100

    initial commit of files

---
+++ mine-project/minepump/periodic_task.c
@@ -0,0 +1,57 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+
+#include "periodic_task.h"
+#include "utils.h"
+
+/*****************************************************************************/
+void *periodic_task_body (void *parameters);
+
+void *periodic_task_body (void *parameters) {
+  struct timespec trigger;                      /* Stores next dispatch time */
+  struct timespec period;                              /* Period of the task */
+  sem_t timer;
+  task_parameters *my_parameters = (task_parameters *) parameters;
+
+  period = (*my_parameters).period;
+
+  sem_init(&timer, 0, 0);
+  clock_gettime(CLOCK_REALTIME, &trigger);
+  for(;;){
+    (*my_parameters).job();
+    add_timespec(&trigger, &trigger, &period);
+    sem_timedwait(&timer, &trigger);
+  }
+
+}
+
+/*****************************************************************************/
+void create_periodic_task (struct timespec period, void (*job) (void)) {
+  pthread_t tid;
+  task_parameters *parameters = malloc (sizeof (task_parameters));
+
+  parameters->period = period;
+  parameters->job = job;
+
+  int status = pthread_create(&tid, NULL, periodic_task_body, (void*) parameters);
+  CHECK_NZ(status);
+
+}
+/************************p t h r e a d*****************************************************/
+#ifdef __TEST__
+void dummy (void) {
+  printf ("o< \n");
+}
+
+int main (void) {
+  struct timespec period;
+  period.tv_nsec = 250 * 1000 * 1000;
+  period.tv_sec  = 0 ;
+
+  create_periodic_task (period, dummy);
+  printf ("Task created\n");
+  while (1);
+  return 0;
+}
+#endif /* __TEST__ */
